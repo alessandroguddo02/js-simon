ceb59741abcdbbc37bdd37c6d876c0a54f7f644d
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-11-12 16:00:20 +0100

    add randomnumbers

--- js/script.js
+++ js/script.js
@@ -9,11 +9,32 @@ const numbers = 5;
 
 //numeri da indovinare
 const numberToGuess = [];
-
+const numberUser = [];
+const commonNumber = [];
+const countdown = document.getElementById("countdown");
+const instruction = document.getElementById("instruction");
 const elUl = document.getElementById("numbers-list");
-
+const answerForm = document.getElementById("answers-form");
+const message = document.getElementById("message");
 let counter = 0;
-let template = ""
+let template = "";
+
+
+function getRndNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 while (numberToGuess.length < 5 && counter < 100){
+   let newLi = document.createElement("li");
+  newLi.innerHTML = getRndNumber(1, 50);
+  counter++;
+  console.log(newLi.innerHTML);
+  if(!numberToGuess.includes(newLi.innerHTML)){
+    numberToGuess.push(newLi.innerHTML);
+    elUl.appendChild(newLi);
+  }
+}
+
+function time (){
     
 }
